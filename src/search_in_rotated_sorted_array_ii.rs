3c6926f029487348de7c99f3c1509e253bcfd299
giwan <giwan.dev@gmail.com>
2022-11-28 17:27:53 +0900

    ✅ Search in Rotated Sorted Array II 성공
    https://leetcode.com/submissions/detail/851081133/

--- src/search_in_rotated_sorted_array_ii.rs
+++ src/search_in_rotated_sorted_array_ii.rs
@@ -1,10 +1,52 @@
+use std::cmp::Ordering;
+
 #[allow(dead_code)]
 fn search(nums: Vec<i32>, target: i32) -> bool {
-    false
+    let first = nums.first().unwrap();
+
+    match first.cmp(&target) {
+        Ordering::Less => {
+            let mut index = 1;
+
+            loop {
+                if index > (nums.len() - 1) {
+                    return false;
+                }
+
+                if nums[index] >= target {
+                    break;
+                }
+
+                index += 1;
+            }
+            nums[index] == target
+        }
+        Ordering::Equal => true,
+        Ordering::Greater => {
+            let mut index = nums.len() - 1;
+
+            loop {
+                if index == 0 {
+                    return false;
+                }
+
+                if nums[index] <= target {
+                    break;
+                }
+
+                index -= 1;
+            }
+
+            nums[index] == target
+        }
+    }
 }
 
 #[test]
 fn basic() {
     assert_eq!(search(vec![2, 5, 6, 0, 0, 1, 2], 0), true);
     assert_eq!(search(vec![2, 5, 6, 0, 0, 1, 2], 3), false);
+    assert_eq!(search(vec![1], 0), false);
+    assert_eq!(search(vec![1], 2), false);
+    assert_eq!(search(vec![1, 1], 0), false);
 }
